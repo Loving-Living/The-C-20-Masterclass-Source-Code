be2f708242917c457e5ed3c1e92cf7790e9f70bf
Nandana <129848120+Loving-Living@users.noreply.github.com>
2024-01-03 20:09:29 +0530

    Update main.cpp

--- 13.Pointers/13.15DynamicMemoryAllocation/main.cpp
+++ 13.Pointers/13.15DynamicMemoryAllocation/main.cpp
@@ -3,8 +3,8 @@
 
 int main(){
 
-	//How we've used pointers so far
-    /*
+//How we've used pointers so far
+    
 	int number {22}; // Stack
 	
 	int * p_number = &number;
@@ -23,43 +23,40 @@ int main(){
 	std::cout << std::endl;
 	std::cout << "Uninitialized pointer : " << std::endl;
 	std::cout << "*p_number1 : " << *p_number1 << std::endl;
-    */
 
    //BAD
-	//Writing into uninitialized pointer through dereference
-    /*
+	// 1) Writing into uninitialized pointer through dereference
+    
 	int *p_number2; // Contains junk address : could be anything
-    std::cout << "Writting in the 55" << std::endl;
+    	std::cout << "Writting in the 55" << std::endl;
 	*p_number2 = 55; // Writing into junk address : BAD!
 	std::cout << std::endl;
 	std::cout << "Writing into uninitialized pointer through dereference" << std::endl;
 	std::cout << "p_number2 : " << p_number2 << std::endl; // Reading from junk address.
-    std::cout << "Dereferencing bad memory" << std::endl;
+    	std::cout << "Dereferencing bad memory" << std::endl;
 	std::cout << "*p_number2 : " << *p_number2 << std::endl;
-    */
 	
 	
-	//Initializing pointer to null
-    /*
+	// 2) Initializing pointer to null
+    
 	//int *p_number3{nullptr}; // Also works
 	int * p_number3 {}; // Initialized with pointer equivalent of zero : nullptr
 						// A pointer pointing nowhere
-    std::cout << "Writting into nullptr memory" << std::endl;
+    	std::cout << "Writting into nullptr memory" << std::endl;
 	//*p_number3 = 33; // Writting into a pointer pointing nowhere : BAD, CRASH
-    std::cout << "Done writting" << std::endl;
+    	std::cout << "Done writting" << std::endl;
 	
 	std::cout << std::endl;
 	std::cout << "Reading and writting through nullptr : " << std::endl;
-	//std::cout << "p_number3 : " << p_number3 << std::endl;
-	//std::cout << "*p_number3 : " << *p_number3 << std::endl;// Reading from nullptr
-															// BAD, CRASH.
-                                                            */
+	////std::cout << "p_number3 : " << p_number3 << std::endl;
+	////std::cout << "*p_number3 : " << *p_number3 << std::endl;// Reading from nullptr
+		//BAD CRASH.
 
 
-    //Dynamic heap memory
-    /*
+	
+//Dynamic heap memory
 	int *p_number4{nullptr};
-	p_number4 = new int; 	// Dynamically allocate space for a single int on the heap
+   p_number4 = new int; 	// Dynamically allocate space for a single int on the heap
 							// This memory belongs to our program from now on. The system
 							// can't use it for anything else, untill we return it.
 							//After this line executes, we will have a valid memory location
@@ -73,13 +70,11 @@ int main(){
 	std::cout <<"*p_number4 : " << *p_number4 << std::endl;
 
     //Return memory to the OS
-    delete p_number4;
-    p_number4 = nullptr;
-    */
+    	delete p_number4;
+   	p_number4 = nullptr;
+    
 
-
-    //It is also possible to initialize the pointer with a valid
-	//address up on declaration. Not with a nullptr
+    //It is also possible to initialize the pointer with a valid address upon declaration. Not with a nullptr
      int *p_number5{ new int}; // Memory location contains junk value
      int *p_number6{ new int (22) }; // use direct initialization
      int *p_number7{ new int { 23 } }; // use uniform initialization
@@ -105,7 +100,39 @@ int main(){
 	  delete p_number7;
 	  p_number7 = nullptr;
 
+/* C uses the malloc() and calloc() function to allocate memory dynamically at run time 
+   and uses a free() function to free dynamically allocated memory. C++ supports these 
+   functions and also has two operators new and delete,that perform the task of 
+   allocating and freeing the memory in a better and easier way.
+   For initializing the memory for built-in data type:
+   
+	pointer-variable = new data-type(value); 
 
+   Allocate a block of memory: 
+     A new operator is also used to allocate a block(an array) of memory of type data type. 
+     		
+       		pointer-variable = new data-type[size];
+
+     where size(a variable) specifies the number of elements in an array. 
+     Example:
+	int *p = new int[10]
+	>>>>Dynamically allocates memory for 10 integers continuously of type int and returns a pointer 
+ 	to the first element of the sequence, which is assigned top(a pointer). p[0] refers to the 
+  	first element, p[1] refers to the second element, and so on. 
+   
+   DELETE
+   	Release memory pointed by pointer-variable
+		delete pointer-variable; 
+  
+  	To free the dynamically allocated array pointed by pointer variable, use the following form of delete: 
+	Release block of memory pointed by pointer-variable
+		delete[] pointer-variable;  
+
+	Example:
+   		 //It will free the entire array pointed by p.
+   			delete[] p;
+   */
+	
       //Can reuse pointers
       p_number5 = new int(81);
       std::cout << "*p_number : " << *p_number5 << std::endl;
@@ -123,7 +150,7 @@ int main(){
       delete p_number5;
 
 
-      //
+      //https://www.geeksforgeeks.org/new-and-delete-operators-in-cpp-for-dynamic-memory/
 
                                                             
    std::cout << "Program is ending well" << std::endl;
