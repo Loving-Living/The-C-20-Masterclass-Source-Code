f8fefbef5563625a34eb4065b308f95742521b1e
Nandana <129848120+Loving-Living@users.noreply.github.com>
2024-01-04 20:12:26 +0530

    Update main.cpp

--- 15.CharacterManipulationAndStrings/15.3CStringManipulation/main.cpp
+++ 15.CharacterManipulationAndStrings/15.3CStringManipulation/main.cpp
@@ -3,34 +3,33 @@
 
 int main(){
 
-    /*
-	//std::strlen : Find the length of a string
+//std::strlen : Find the length of a string
     // real arrays and those decayed into pointers
-    const char message1 [] {"The sky is blue."};
+    const char message1 [] {"Hello"};
 	
-	//Array decays into pointer when we use const char*
-	const char* message2 {"The sky is blue."};
+   //Array decays into pointer when we use const char*
+    const char* message2 {"Hello"};
     std::cout << "message1 : " << message1 << std::endl;
 	
 	//strlen ignores null character
-    std::cout << "strlen(message1) : " << std::strlen(message1) << std::endl;
+    std::cout << "strlen(message1) : " << std::strlen(message1) << std::endl;	//5
 	
 	// Includes the null character
-    std::cout << "sizeof(message1) : " << sizeof(message1) << std::endl;
+    std::cout << "sizeof(message1) : " << sizeof(message1) << std::endl<< std::size(message);//66
 	
 	//strlen still works with decayed arrays
-	std::cout << "strlen(message2) : " << std::strlen(message2) << std::endl;
+	std::cout << "strlen(message2) : " << std::strlen(message2) << std::endl;//5//std::size gives ERROR
 	
 	//Prints size of pointer
-    std::cout << "sizeof(message2) : " << sizeof(message2) << std::endl;
-    */
+    	std::cout << "sizeof(message2) : " << sizeof(message2) << std::endl;	//8 always
+    
 
 
-   //std::strcmp - signature : int strcmp( const char *lhs, const char *rhs );
+//std::strcmp - signature : int strcmp( const char *lhs, const char *rhs );
     //Returns negative value if lhs appears before rhs in lexicographical order,
     //Zero if lhs and rhs compare equal.
     //and Positive value if lhs appears after rhs in lexicographical order. 
-    /*
+    
     std::cout << std::endl;
     std::cout << "std::strcmp : " << std::endl;
     const char* string_data1{ "Alabama" };
@@ -41,7 +40,7 @@ int main(){
 
     //Print out the comparison
     std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
-        << std::strcmp(string_data1, string_data2) << std::endl;
+        << std::strcmp(string_data1, string_data2) << std::endl;//-1
 
     std::cout << "std::strcmp (" << string_data3 << "," << string_data4 << ") : "
         << std::strcmp(string_data3, string_data4) << std::endl;
@@ -81,7 +80,7 @@ int main(){
     */
 
 
-	//std::strncmp : int strncmp( const char *lhs, const char *rhs, std::size_t count );
+//std::strncmp : int strncmp( const char *lhs, const char *rhs, std::size_t count );
 	//Compares n characters in the strings
 	//Returns : Negative value if lhs appears before rhs in lexicographical order.
     //Zero if lhs and rhs compare equal, or if count is zero.
