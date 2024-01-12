06bb07ea58cdce1946bcd0f9e888efe41e451634
Nandana <129848120+Loving-Living@users.noreply.github.com>
2024-01-12 19:34:41 +0530

    Update main.cpp

--- 20.FunctionOverloading/20.2OverloadingWithDifferentParameters/main.cpp
+++ 20.FunctionOverloading/20.2OverloadingWithDifferentParameters/main.cpp
@@ -6,27 +6,27 @@ int max(int a, int b){
     return (a>b)? a : b; 
 }
 
-double max(double a, double b){
+double max(double a, double b){    //Diff type
     std::cout << "double overload called" << std::endl;
     return (a>b)? a : b; 
 }
 
-double max(int a, double b){
+double max(int a, double b){    //Diff type
     std::cout << "(int,double) overload called" << std::endl;
     return (a>b)? a : b; 
 }
 
-double max(double a, int b){
+double max(double a, int b){    //Diff order
     std::cout << "(double,int) overload called" << std::endl;
     return (a>b)? a : b; 
 }
 
-double max(double a, int b,int c){
+double max(double a, int b,int c){    //Diff no of parameters
     std::cout << "(double,int,int) overload called" << std::endl;
     return a;
 }
 
-std::string_view  max(std::string_view a, std::string_view b){
+std::string_view  max(std::string_view a, std::string_view b){    //Diff type of parameters
     std::cout << "(string_view,string_view) overload called" << std::endl;
      return (a>b)? a : b;
 }
